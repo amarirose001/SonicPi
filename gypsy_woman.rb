7c6a2147b68439a90cf5e40057896d0038de98c8
amarirose001 <124717131+amarirose001@users.noreply.github.com>
2023-03-10 11:35:20 -0800

    Update gypsy_woman.rb

--- gypsy_woman.rb
+++ gypsy_woman.rb
@@ -1,5 +1,74 @@
 # Gypsy Woman
 
+beep = "C:\Users\Amari Rose-Aminifu\Downloads\video game beep-Sound effect.wav"
+
+notes = [:A1,:E2,:Bb1,:F2,:C2,:Ab1,:D2,:C2,:A1,:C2]
+
+define :clap do
+  sleep 1
+  sample :perc_impact2, amp: 0.3
+  sleep 1
+end
+
+x = 0.3
+
+y = 0.8
+
+use_bpm 120
+use_synth :fm
+define :amps do
+  play:D5, amp: x
+  play:C5, amp: x
+  play:A4, amp: x
+  play:F4, amp: x
+  sleep 0.75
+end
+
+use_bpm 120
+use_synth :fm
+define :ampd do
+  play:D5, amp: y
+  play:C5, amp: y
+  play:A4, amp: y
+  play:F4, amp: y
+  sleep 0.75
+end
+
+sample beep
+
+use_bpm 120
+use_synth :fm
+with_fx :band_eq do
+  play:E5, amp: 0.1
+  play:C5, amp: 0.1
+  play:A4, amp: 0.1
+  play:F4, amp: 0.1
+  sleep 1
+  play:E5, amp: 0.2
+  play:C5, amp: 0.2
+  play:A4, amp: 0.2
+  play:F4, amp: 0.2
+  sleep 0.75
+  3.times do
+    amps
+    x = x + 0.1
+  end
+  play:E5, amp: 0.6
+  play:C5, amp: 0.6
+  play:A4, amp: 0.6
+  play:F4, amp: 0.6
+  sleep 1
+  play:E5, amp: 0.7
+  play:C5, amp: 0.7
+  play:A4, amp: 0.7
+  play:F4, amp: 0.7
+  sleep 0.75
+  3.times do
+    ampd
+    y = y + 0.1
+  end
+end
+
 use_bpm 120
 use_synth :fm
 live_loop:high_notes do
@@ -336,7 +405,7 @@ end
 
 sleep 40
 
-live_loop:background_drum1 do
+live_loop:background_drum do
   sample :drum_bass_soft, amp: 2
   sleep 0.5
   sample :drum_cymbal_closed
@@ -348,12 +417,10 @@ live_loop:background_drum1 do
 end
 
 live_loop:background_clap do
-  sleep 1
-  sample :perc_impact2, amp: 0.3
-  sleep 1
+  clap
 end
 
-live_loop:background_cymbal do
+live_loop:background_c do
   sleep 1.85
   sample :drum_cymbal_closed, rate: 1.5
   sleep 0.15
@@ -366,24 +433,24 @@ end
 sleep 8
 
 live_loop:background_notes do
-  play:A1, amp: 2
+  play notes[0], amp: 2
   sleep 1
-  play:E2, amp: 2
+  play notes[1], amp: 2
   sleep 1
-  play:Bb1, amp: 2
+  play notes[2], amp: 2
   sleep 1
-  play:F2, amp: 2
+  play notes[3], amp: 2
   sleep 0.5
-  play:C2, amp: 2
+  play notes[4], amp: 2
   sleep 0.5
-  play:Ab1, amp: 2
+  play notes[5], amp: 2
   sleep 1
-  play:D2, amp: 2
+  play notes[6], amp: 2
   sleep 1
-  play:C2, amp: 2
+  play notes[7], amp: 2
   sleep 1
-  play:A1, amp: 2
+  play notes[8], amp: 2
   sleep 0.5
-  play:C2, amp: 2
+  play notes[9], amp: 2
   sleep 0.5
 end
